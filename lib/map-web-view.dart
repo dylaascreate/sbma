3fedd471ecc6a0c1fa5ce9030009e4ec5bc30a7f
fdlhhmd <d20221101818@siswa.upsi.edu.my>
2025-07-05 15:46:26 +0800

    updated map page

--- lib/map-web-view.dart
+++ lib/map-web-view.dart
@@ -12,7 +12,7 @@ class _EmbeddedMapPageState extends State<EmbeddedMapPage> {
    final controller = WebViewController()
     ..setJavaScriptMode(JavaScriptMode.unrestricted)
     ..loadRequest(
-  Uri.parse('https://www.google.com/maps/embed/v1/place?q=Sultan+Idris+Education+University&key=AIzaSyD3dRzOt9YhFMPqu_qtT3bMr3gp7hnaTJE'),
+  Uri.parse('https://www.google.com/maps/embed/v1/place?q=Sultan+Idris+Education+University&key=AIzaSyDEXUQ2iJzMyyTcWEH7up_nL_oCoNk-Kv8'),
 );
 
 
@@ -29,3 +29,49 @@ class _EmbeddedMapPageState extends State<EmbeddedMapPage> {
     );
   }
 }
+
+// v2
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// class NativeMapPage extends StatefulWidget {
+//   const NativeMapPage({super.key});
+
+//   @override
+//   State<NativeMapPage> createState() => _NativeMapPageState();
+// }
+
+// class _NativeMapPageState extends State<NativeMapPage> {
+//   late GoogleMapController mapController;
+
+//   final LatLng _upsiLocation = const LatLng(3.7150, 101.5420); // UPSI coords
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('UPSI Native Map'),
+//         backgroundColor: const Color(0xFFFFA000),
+//         centerTitle: true,
+//       ),
+//       body: GoogleMap(
+//         onMapCreated: (controller) => mapController = controller,
+//         initialCameraPosition: CameraPosition(
+//           target: _upsiLocation,
+//           zoom: 16,
+//         ),
+//         markers: {
+//           Marker(
+//             markerId: const MarkerId('upsi'),
+//             position: _upsiLocation,
+//             infoWindow: const InfoWindow(
+//               title: 'Universiti Pendidikan Sultan Idris',
+//             ),
+//           ),
+//         },
+//         myLocationButtonEnabled: true,
+//         zoomControlsEnabled: true,
+//       ),
+//     );
+//   }
+// }
